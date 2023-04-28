ffc420645fbcf0a7de5b3216b7fc9219d0dea40d
Edixon Gomez <edixongomez6@gmail.com>
2023-04-28 17:19:05 -0400

    practica

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+let variable1: string = "Edixon Gomez"
+const variable2: number = 2023
+let variable3: boolean = true
+let variable4: null 
+let variable5: any = "trtrt"
+
+let variable6: Array<any> = [
+    3,
+    5,
+    7,
+    0,
+    "Edixon Gomez"
+    ]
+let variable7: any = 
+console.log(variable1 + variable3)
